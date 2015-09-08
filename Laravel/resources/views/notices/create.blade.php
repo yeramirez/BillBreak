abeb79ce743291948b924461f2c465eda9ea8821
yeramirez <yeramirez@fullsail.edu>
2015-09-08 19:24:34 -0400

    Commit three

--- Laravel/resources/views/notices/create.blade.php
+++ Laravel/resources/views/notices/create.blade.php
@@ -5,19 +5,24 @@
     <div class="container-fluid col-md-6 create">
         <h3 class="">Add Roommate</h3>
 
-        {!! Form::open(['method' => 'GET', 'action' => 'NoticesController@confirm'], ['class' => 'col-md-8']) !!}
+        {!! Form::open(['method' => 'POST', 'action' => 'RoommateController@create'], ['class' => 'col-md-8']) !!}
         <div class="form-group">
-            {!! Form::label('inviteRoommate','Roommates Name: ') !!}
-            {!! Form::text('inviteRoommate',null,['class' => 'form-control']) !!}
+            {!! Form::label('rmName','Roommates Name: ') !!}
+            {!! Form::text('rmName',null,['class' => 'form-control']) !!}
         </div>
 
         <div class="form-group">
-            {!! Form::label('inviteEmail','Roommate Email: ') !!}
-            {!! Form::text('inviteEmail',null,['class' => 'form-control']) !!}
+            {!! Form::label('rmEmail','Roommate Email: ') !!}
+            {!! Form::text('rmEmail',null,['class' => 'form-control']) !!}
         </div>
 
         <div class="form-group">
-            {!! Form::submit('Preview', ['class' => 'btn btn-primary']) !!}&nbsp;&nbsp;
+            {!! Form::label('rmPassword','Create Roommate Password: ') !!}<br>
+            {!! Form::password('rmPassword',null,['class' => 'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Register', ['class' => 'btn btn-primary']) !!}&nbsp;&nbsp;
             <?php echo link_to_action('PagesController@dashboard', $title = 'Back to Dashboard', $parameters = array(), $attributes = array()); ?>
         </div>
         {!! Form::close() !!}
