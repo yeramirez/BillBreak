0236ea8f607b5bdf09eb2c995e2fe6a0fb6e4437
yeramirez <yeramirez@fullsail.edu>
2015-09-08 15:02:52 -0400

    Fixed authorization to dashboard

--- Laravel/app/Http/routes.php
+++ Laravel/app/Http/routes.php
@@ -5,7 +5,11 @@ Route::get('/','PagesController@index');
 Route::get('contact','PagesController@contact');
 Route::get('notices/create/confirm','NoticesController@confirm');
 Route::resource('notices', 'NoticesController');
-Route::get("/dashboard", "PagesController@dashboard", ['middleware' => 'auth']);
+
+Route::any('dashboard', [
+    'middleware' => 'auth',
+    'uses' => 'PagesController@dashboard'
+]);
 
 
 Route::controllers([
--- Laravel/resources/views/app.blade.php
+++ Laravel/resources/views/app.blade.php
@@ -10,7 +10,7 @@
     
 	<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/paper/bootstrap.min.css" rel="stylesheet" integrity="sha256-hMIwZV8FylgKjXnmRI2YY0HLnozYr7Cuo1JvRtzmPWs= sha512-k+wW4K+gHODPy/0gaAMUNmCItIunOZ+PeLW7iZwkDZH/wMaTrSJTt7zK6TGy6p+rnDBghAxdvu1LX2Ohg0ypDw==" crossorigin="anonymous">
 	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" type="text/css" href="sass/main.css" >
+	<link rel="stylesheet" type="text/css" href="/public/sass/main.css" >
     <link rel="icon" type="image/png" href="img/favicon.png">
 
 	<title>Bill Break</title>
